493efc0677227a255bb33678950c232cc0a5f4af
Rom1009 <quynguyencurit2002@gmail.com>
2025-02-21 18:58:16 +0700

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://Rom1009.github.io',
-    base: 'my-repo',
+    base: 'Portfolio',
     output: 'static'
 });
